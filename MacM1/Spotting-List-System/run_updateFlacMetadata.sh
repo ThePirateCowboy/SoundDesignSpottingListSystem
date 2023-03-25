a6d94671a6462eebca546bd63583cafc9fab693d
Ben Harding <ben@papermountain.games>
2023-03-25 19:04:59 +0000

    Deleted & cleaned up repo

--- MacM1/Spotting-List-System/run_updateFlacMetadata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source "/Users/benharding/Library/Application Support/REAPER/Scripts/SoundDesignSpottingListSystem/MacM1/Spotting-List-System/my_virtual_environment_SDSL/bin/activate"
-python  "/Users/benharding/Library/Application Support/REAPER/Scripts/SoundDesignSpottingListSystem/Python Scripts/update_flac_metadata.py"
