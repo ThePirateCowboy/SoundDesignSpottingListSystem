950ba9e22e250fb6c5a5f1aedc577020911a0b36
Ben Harding <ben@papermountain.games>
2023-03-25 13:14:53 +0000

    M1

---
+++ MacM1/Spotting-List-System/run_updateFlacMetadata.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source "/Users/benharding/Library/Application Support/REAPER/Scripts/SoundDesignSpottingListSystem/MacM1/Spotting-List-System/my_virtual_environment_SDSL/bin/activate"
+python  "/Users/benharding/Library/Application Support/REAPER/Scripts/SoundDesignSpottingListSystem/Python Scripts/update_flac_metadata.py"
